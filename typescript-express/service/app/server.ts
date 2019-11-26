5b5c830969e28ff2f4caa9e2e356e79f549d0e1b
Rishichandra Wawhal <rishi@hasura.io>
2019-11-26 12:20:41 +0530

    change root path of webhooks

--- typescript-express/service/app/server.ts
+++ typescript-express/service/app/server.ts
@@ -6,7 +6,7 @@ const app: express.Application = express();
 const port: number = Number(process.env.PORT) || 3000;
 
 app.use(bodyParser.json());
-app.use('/actions', WelcomeController);
+app.use('/', WelcomeController);
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}/`);
