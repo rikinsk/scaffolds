336ac53c936721cba990b66b77ba43b2f0b4fb9a
rikinsk <rikin.kachhia@gmail.com>
2019-11-30 17:43:43 +0530

    fix nodejs express server.js

--- nodejs-express/service/src/server.js
+++ nodejs-express/service/src/server.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 4000;
 
 const app = new express();
 
-app.use(router);
 app.use(bodyParser.json());
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Listening at ${PORT}`);
